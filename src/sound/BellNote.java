23644cd1cca5f1c59634fd02d03b9ac1e6964a37
CanFam23 <nickclouse03@gmail.com>
2025-03-22 16:38:43 -0600

    Added toString, equals, hashCode methods to BellNote class

--- src/sound/BellNote.java
+++ src/sound/BellNote.java
@@ -1,5 +1,7 @@
 package sound;
 
+import java.util.Objects;
+
 public class BellNote {
     private final Note note;
     private final NoteLength length;
@@ -15,5 +17,22 @@ public class BellNote {
 
     public NoteLength getLength() {
         return length;
+    }
+
+    public String toString() {
+        return "Note: " + note + " | Length: " + length;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BellNote bellNote = (BellNote) o;
+        return note == bellNote.note && length == bellNote.length;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(note, length);
     }
 }
