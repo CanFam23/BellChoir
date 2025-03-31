2b55748d712f55bbb4b6d9866fc05a635da8b431
CanFam23 <nickclouse03@gmail.com>
2025-03-30 18:29:35 -0600

    Updated and corrected conductor class to work with new member class, actually controls members now

--- src/main/Conductor.java
+++ src/main/Conductor.java
@@ -7,10 +7,6 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import java.util.*;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.locks.ReentrantLock;
 
 /*
 TODO:
@@ -20,31 +16,28 @@ TODO:
  - More validation
  - Starvation checks, make sure no member is waiting too long
  - Dead lock checks?
- - race conditions should be ok
  */
 
 public class Conductor implements Runnable{
     private final Map<BellNote, Member> members = new HashMap<>();
 
-    private final BlockingQueue<BellNote> songOrder = new LinkedBlockingQueue<>();
+    private final List<BellNote> song;
 
-    private int numMembers = 0;
-
-    private final ReentrantLock lock = new ReentrantLock(true);
-
-    private final CountDownLatch latch = new CountDownLatch(1);
-
-    private final SourceDataLine line;
+    private SourceDataLine line;
 
     private final Thread thread;
 
-    public Conductor(AudioFormat af) {
-        try {
-            this.line = AudioSystem.getSourceDataLine(af);
-        } catch (LineUnavailableException e) {
-            throw new RuntimeException(e);
-        }
+    private int numMembers = 0;
+
+    public Conductor(AudioFormat af,List<BellNote> song) {
         this.thread = new Thread(this, "Conductor");
+        this.song = song;
+
+        setLine(af);
+
+        for (BellNote b : this.song) {
+            addMember(b);
+        }
     }
 
     public static void main(String[] args) {
@@ -70,73 +63,114 @@ public class Conductor implements Runnable{
             System.exit(1);
         }
 
+        // Create the audio format
         final AudioFormat af =
                 new AudioFormat(Note.SAMPLE_RATE, 8, 1, true, false);
-        Conductor conductor = new Conductor(af);
 
-        conductor.createChoir(song);
+        final Conductor conductor = new Conductor(af,song);
 
-        conductor.start();
+        // Play the song
+        conductor.playSong();
 
+        // Stop the conductor thread (will stop all members as well once song is over)
         conductor.stop();
     }
 
-    public void start(){
+    public void playSong() {
+        // Open and start the line, if unavailable, wait 3 seconds then try again
         try {
             line.open();
+            line.start();
+            // If line is not available, wait 3 seconds and try again
         } catch (LineUnavailableException e) {
-            throw new RuntimeException(e);
-        }
-        line.start();
-        thread.start();
+            System.err.println("Line was unavailable when trying to open/start it, waiting 3 seconds and trying again...");
 
+            // Wait 3 seconds
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ignored) {}
+
+            // If line is still unavailable after 3 seconds, terminate program
+            try {
+                line.open();
+                line.start();
+            } catch (LineUnavailableException lue) {
+                System.err.println("Line still unavailable when trying to open/start it after waiting 3 seconds, aborting");
+                System.exit(1);
+            }
+
+        }
+
+        thread.start();
+    }
+
+    public void startMembers(){
         for (Member member : members.values()) {
             member.start();
         }
-        latch.countDown();
     }
 
     /**
-     * Runs this operation.
+     * Opens and starts the line, starts the member threads, and plays the song.
      */
     @Override
     public void run() {
-        while(!songOrder.isEmpty()) {
-//            for (Member member : members.values()) {
-//                System.out.println(member.getState());
-//            }
-//            System.out.println();
+        startMembers();
+
+        // Loop through all the notes in the song and have the member that plays the given note play it.
+        for(BellNote b : song) {
+            Member member = members.get(b);
+
+            // If no member exists in the hashMap, add it then assign it to member so it plays the note
+            if (member == null) {
+                addMember(b);
+                member = members.get(b);
+            }
+
+            member.giveTurn();
         }
     }
 
     public void stop(){
-        try {
+        try{
             thread.join();
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            System.err.println("Thread was interrupted while waiting for conductor thread to finish.");
         }
         stopMembers();
         line.drain();
         line.close();
     }
 
-    public void createChoir(List<BellNote> song) {
-        songOrder.addAll(song);
-
-        for (BellNote b : song) {
-            addMember(b);
-        }
-    }
-
     public void addMember(BellNote b) {
         if (!members.containsKey(b)) {
-            members.put(b,new Member(b,numMembers++,lock,songOrder,line,latch));
+            members.put(b,new Member(numMembers++,b,line));
         }
     }
 
     public void stopMembers() {
         for (Member member : members.values()) {
             member.stop();
+        }
+    }
+
+    private void setLine(AudioFormat af) {
+        try {
+            this.line = AudioSystem.getSourceDataLine(af);
+            // If line is not available, wait 3 seconds and try again
+        } catch (LineUnavailableException e) {
+            System.err.println("Line was unavailable, waiting 3 seconds and trying again...");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ignored) {}
+
+            // If line is still unavailable after 3 seconds, terminate program
+            try {
+                this.line = AudioSystem.getSourceDataLine(af);
+            } catch (LineUnavailableException lue) {
+                System.err.println("Line still unavailable after waiting 3 seconds, aborting");
+                System.exit(1);
+            }
         }
     }
 }
